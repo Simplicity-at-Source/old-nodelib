f492a5e4cc8dc6fe5bd860e050e55f0edad2aeab
fuzzy-logic <spam.gawain@gmail.com>
2014-07-08 17:51:49 +0100

    added test scripts and vars

--- test/testMuonClient.js
+++ test/testMuonClient.js
@@ -4,7 +4,7 @@ var url = require('url');
 var muon = require("../munode.js");
 var nucleus = require("./muon-test.js");
 
-var muonPort = 7777;
+var muonPort = 18081;
 
 
 
@@ -21,42 +21,38 @@ describe("test muon-node client ", function() {
             { recordId:"proxy", inspection: { NetworkSettings: { IPAddress:"111.111.111.88" }}},
         ]);
 
+        //TODO, the standard containers ...
+        //expect a response message of the correct form, including data from the docker api.
 
+        muon.on(function (event) {
+            assert.equal(event.resource, "container");
+            assert.equal(event.action, "put");
+            assert.equal(event.type, "runtime");
+            assert.equal(event.recordId, "simplenode");
+            console.dir(event.payload);
+            //    payload:actions[6]
+            muon.shutdown();
+            done()
+        });
 
-        function exec() {
-            //TODO, the standard containers ...
-
-            //expect a response message of the correct form, including data from the docker api.
-
-            muon.on(function (event) {
-                assert.equal(event.resource, "container");
-                assert.equal(event.action, "put");
-                assert.equal(event.type, "runtime");
-                assert.equal(event.recordId, "simplenode");
-                console.dir(event.payload);
-                //    payload:actions[6]
-                muon.shutdown();
-                done()
-            });
-
-            //tell mock muon to emit a PUT.
-            muon.send({
-                resource: "container",
-                type: "gene",
-                recordId:"simplenode",
-                action: "put",
-                payload: {
-                    "id": "simplenode",
-                    "image": "sp_platform/uber-any",
-                    "env": {
-                        "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git",
-                        "DNSHOST": "simplenode.muon.cistechfutures.net"
-                    }
+        //tell mock muon to emit a PUT.
+        muon.send({
+            resource: "container",
+            type: "gene",
+            recordId:"simplenode",
+            action: "put",
+            payload: {
+                "id": "simplenode",
+                "image": "sp_platform/uber-any",
+                "env": {
+                    "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git",
+                    "DNSHOST": "simplenode.muon.cistechfutures.net"
                 }
-            });
-        }
+            }
+        });
+        
         //quite crappy. can't quite get to the low level stuff needed to properly event this.
-        setTimeout(exec, 100);
+        setTimeout(done, 3000);
     });  
 
 });
