2d5bf9e3b752b923818300d33b6bd43a6d7463f4
David Dawson <david.dawson@simplicityitself.com>
2014-07-08 19:19:04 +0100

    Fix port

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@
 export NODE_DEBUG=true
 export SP_DOCKER_PORT=14321
 export SP_DOCKER_HOST=localhost
-export MUON_NUCLEUS_PORT=18081
+export MUON_NUCLEUS_PORT=7777
 export MUON_NUCLEUS_HOST=localhost
 export MUON_DOMAIN=dev.muon.io
 
