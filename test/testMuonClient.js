000be5349b5b6708af3b658f4ea306cf8b115179
David Dawson <david.dawson@simplicityitself.com>
2014-07-08 18:29:21 +0100

    Working

--- test/testMuonClient.js
+++ test/testMuonClient.js
@@ -3,8 +3,11 @@ var muonPort = 7777;
 
 var assert = require('assert');
 var url = require('url');
-var muon = require("../munode.js")();
 
+var nucleus = require("./muon-test.js");
+nucleus.listen(muonPort);
+
+var muon = require("../munode.js")();
 
 describe("test muon-node client ", function() {
     
@@ -12,43 +15,41 @@ describe("test muon-node client ", function() {
 
     it ("basic test", function(done) {
         this.timeout(3500);
-        
-        var nucleus = require("./muon-test.js");
-
-        nucleus.listen(muonPort);
 
         //TODO, the standard containers ...
         //expect a response message of the correct form, including data from the docker api.
 
-        nucleus.on(function (event) {
+	var sender = function() {
+	  nucleus.on(function (event) {
             assert.equal(event.resource, "container");
             assert.equal(event.action, "put");
-            assert.equal(event.type, "runtime");
+            assert.equal(event.type, "gene");
             assert.equal(event.recordId, "simplenode");
             console.dir(event.payload);
             //    payload:actions[6]
-            muon.shutdown();
+           // muon.shutdown();
             done()
-        });
-
-        //tell mock muon to emit a PUT.
-        muon.send({
-            resource: "container",
-            type: "gene",
-            recordId:"simplenode",
-            action: "put",
-            payload: {
-                "id": "simplenode",
-                "image": "sp_platform/uber-any",
-                "env": {
-                    "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git",
-                    "DNSHOST": "simplenode.muon.cistechfutures.net"
-                }
-            }
-        });
-        
+	  });
+	  
+	  //tell mock muon to emit a PUT.
+	  muon.emit({
+	      resource: "container",
+	      type: "gene",
+	      recordId:"simplenode",
+	      action: "put",
+	      payload: {
+		  "id": "simplenode",
+		  "image": "sp_platform/uber-any",
+		  "env": {
+		      "GIT_REPO_URL": "https://github.com/fuzzy-logic/simplenode.git",
+		      "DNSHOST": "simplenode.muon.cistechfutures.net"
+		  }
+	      }
+	  });
+	}
+	
         //quite crappy. can't quite get to the low level stuff needed to properly event this.
-        setTimeout(done, 3000);
+        setTimeout(sender, 100);
     });  
 
 });
